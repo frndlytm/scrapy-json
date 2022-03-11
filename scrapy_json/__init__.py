d4ba8e0687e190600d8431965dc1573c00cd8be0
Christian DiMare <frndlytm@gmail.com>
2022-03-10 23:56:11 -0500

    Barebones implementation.

--- scrapy_json/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Scrapy JSON."""
-
-__author__ = """Christian DiMare-Baits"""
-__email__ = 'frndlytm@gmail.com'
-__version__ = '0.1.0'
--- tests/test_scrapy_json.py
+++ tests/test_scrapy_json.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 """Tests for `scrapy_json` package."""
+import json
 
 import pytest
 
-from click.testing import CliRunner
-
-from scrapy_json import scrapy_json
-from scrapy_json import cli
+from scrapy_json import JsonResponse
 
 
 @pytest.fixture
@@ -18,20 +16,43 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    text = json.dumps({
+        "books": [
+            {
+                "category": "reference",
+                "author": "Nigel Rees",
+                "title": "Sayings of the Century",
+                "isbn": "6-246-2356-8",
+                "price": 8.95
+            },
+            {
+                "category": "fiction",
+                "author": "Evelyn Waugh",
+                "title": "Sword of Honour",
+                "isbn": "5-444-34234-8",
+                "price": 12.99
+            },
+            {
+                "category": "fiction",
+                "author": "Herman Melville",
+                "title": "Moby Dick",
+                "isbn": "0-553-21311-3",
+                "price": 8.99
+            },
+            {
+                "category": "fiction",
+                "author": "J. R. R. Tolkien",
+                "title": "The Lord of the Rings",
+                "isbn": "0-395-19395-8",
+                "price": 22.99
+            }
+        ]
+    }).encode("utf-8")
+    return JsonResponse(body=text, url="http://localhost:5000")
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'scrapy_json.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert len(response.jsonpath("books[*].isbn")) == 4
